ee3c2f90d00de4559fa857f834d94f6318ec63fe
swiewior <sbswwr+bitbucket@gmail.com>
2017-12-13 20:11:50 +0100

    TTS Settings

--- app/src/main/java/swiewiora/ttsnotifier/MainActivity.java
+++ app/src/main/java/swiewiora/ttsnotifier/MainActivity.java
@@ -7,12 +7,15 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -159,6 +162,7 @@ public class MainActivity extends AppCompatPreferenceActivity {
         loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
+
     /**
      * This method stops fragment injection in malicious applications.
      * Make sure to deny any unknown fragments here.
@@ -274,6 +278,85 @@ public class MainActivity extends AppCompatPreferenceActivity {
      * activity is showing a two-pane settings UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class TTSPreferenceFragment extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_tts);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+//            bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
+
+            Preference pTTS = findPreference(getString(R.string.key_ttsSettings));
+            Intent ttsIntent = getTtsIntent();
+            if (ttsIntent != null) {
+                pTTS.setIntent(ttsIntent);
+            } else {
+                pTTS.setEnabled(false);
+                pTTS.setSummary(R.string.tts_settings_summary_fail);
+            }
+            EditTextPreference pTtsString = (EditTextPreference)findPreference(getString(R.string.key_ttsString));
+            if (pTtsString.getText().contains("%")) {
+                Toast.makeText(getActivity(), R.string.tts_message_reset_default, Toast.LENGTH_LONG).show();
+                pTtsString.setText(getString(R.string.ttsString_default_value));
+            }
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        private Intent getTtsIntent() {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            if (checkActivityExist("com.android.settings.TextToSpeechSettings")) {
+                intent.setClassName("com.android.settings", "com.android.settings.TextToSpeechSettings");
+            } else if (checkActivityExist("com.android.settings.Settings$TextToSpeechSettingsActivity")) {
+                intent.setClassName("com.android.settings", "com.android.settings.Settings$TextToSpeechSettingsActivity");
+            } else if (checkActivityExist("com.google.tv.settings.TextToSpeechSettingsTop")) {
+                intent.setClassName("com.google.tv.settings", "com.google.tv.settings.TextToSpeechSettingsTop");
+            } else return null;
+            return intent;
+        }
+
+        private boolean checkActivityExist(String name) {
+            try {
+                PackageInfo pkgInfo = getActivity().getPackageManager().getPackageInfo(
+                        name.substring(0, name.lastIndexOf(".")), PackageManager.GET_ACTIVITIES);
+                if (pkgInfo.activities != null) {
+                    for (int n = 0; n < pkgInfo.activities.length; n++) {
+                        if (pkgInfo.activities[n].name.equals(name)) return true;
+                    }
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
+            if (key.equals(getString(R.string.key_ttsStream))) {
+                Common.setVolumeStream(getActivity());
+            }
+        }
+
+    }
+
+    /**
+     * This fragment shows notification preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class NotificationPreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -286,36 +369,6 @@ public class MainActivity extends AppCompatPreferenceActivity {
             // updated to reflect the new value, per the Android Design
             // guidelines.
 //            bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), MainActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * This fragment shows notification preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class TTSPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_tts);
-            setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
         }
 
         @Override
